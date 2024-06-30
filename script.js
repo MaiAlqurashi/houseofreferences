6469cdf7e4e7ea1ad6c2d76e94675f7d01004989
May Alqurashi <114152104+MaiAlqurashi@users.noreply.github.com>
2024-06-30 20:52:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var splashScreen = document.getElementById("splash-screen");
+    setTimeout(function() {
+      splashScreen.classList.add("hide");
+    }, 3000); // Hide splash screen after 3 seconds (adjust as needed)
+  });
+
+
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.pageYOffset > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+
+
+  const carousel = document.querySelector(".carousel");
+  const arrowBtns = document.querySelectorAll(".wrapper i");
+  const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+  const carouselChildrens = [...carousel.children];
+
+  let isDragging = false, startX, startScrollLeft;
+
+  let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+  carouselChildrens.slice(-cardPerView).reverse().forEach(card =>{
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+  carouselChildrens.slice(0, cardPerView).forEach(card =>{
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+  });
+
+  arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () =>{
+      carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+  });
+
+  const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX
+    startScrollLeft = carousel.scrollLeft;
+  }
+
+  const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+  }
+
+  const dragStop = () =>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
+  }
+
+  const infiniteScroll = () => {
+    if(carousel.scrollLeft === 0){
+      carousel.classList.add("no-transition");
+      carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+      carousel.classList.remove("no-transition");
+    }
+
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+      carousel.classList.add("no-transition");
+      carousel.scrollLeft = carousel.offsetWidth;
+      carousel.classList.remove("no-transition");
+    }
+  }
+
+
+  carousel.addEventListener("mousedown", dragStart);
+  carousel.addEventListener("mousemove", dragging);
+  document.addEventListener("mouseup", dragStop);
+  carousel.addEventListener("scroll", infiniteScroll);
