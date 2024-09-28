d5309b2f5f1eb2c0324a432b68be164527048ae0
May Alqurashi <114152104+MaiAlqurashi@users.noreply.github.com>
2024-09-28 16:23:33 +0300

    back

--- script.js
+++ script.js
@@ -76,9 +76,4 @@ document.addEventListener("DOMContentLoaded", function() {
   document.addEventListener("mouseup", dragStop);
   carousel.addEventListener("scroll", infiniteScroll);
 
-  function toggleMenu() {
-    const navLinks = document.getElementById("nav-links");
-    const hamburger = document.getElementById("hamburger");
-    navLinks.classList.toggle("active");
-    hamburger.classList.toggle("active");
-}
+  
